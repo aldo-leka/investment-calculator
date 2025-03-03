4a47ee934aef746b507aaaa63ff968fc6722e4ec
Aldo <aldo.leka@live.com>
2025-03-03 14:20:56 +0100

    Lecture 98 - Add Academind App.jsx & Header.jsx

---
+++ src/AppAcademind.jsx
@@ -0,0 +1,9 @@
+import Header from "./components-academind/Header.jsx";
+
+function AppAcademind() {
+    return (
+        <Header />
+    );
+}
+
+export default AppAcademind
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
-import App from './App.jsx';
 import './index.css';
+import AppAcademind from "./AppAcademind.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<AppAcademind />);
