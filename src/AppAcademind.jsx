0376d8fb8795d0fde69937f16c77f9903ce11e62
Aldo <aldo.leka@live.com>
2025-03-03 16:09:32 +0100

    Lecture 100 - Complete UserInput.jsx

--- src/AppAcademind.jsx
+++ src/AppAcademind.jsx
@@ -1,8 +1,12 @@
 import Header from "./components-academind/Header.jsx";
+import UserInput from "./components-academind/UserInput.jsx";
 
 function AppAcademind() {
     return (
-        <Header />
+        <>
+            <Header/>
+            <UserInput/>
+        </>
     );
 }
 
