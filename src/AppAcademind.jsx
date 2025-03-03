4b692d9007299d38544595340f550fd0502e76ff
Aldo <aldo.leka@live.com>
2025-03-03 16:17:48 +0100

    Lecture 101 - Lifting userInput state up to AppAcademind.jsx

--- src/AppAcademind.jsx
+++ src/AppAcademind.jsx
@@ -1,11 +1,31 @@
 import Header from "./components-academind/Header.jsx";
 import UserInput from "./components-academind/UserInput.jsx";
+import {useState} from "react";
+import Result from "./components/Result.jsx";
+import Results from "./components-academind/Results.jsx";
 
 function AppAcademind() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            }
+        });
+    }
+
     return (
         <>
             <Header/>
-            <UserInput/>
+            <UserInput userInput={userInput} onChange={handleChange} />
+            <Results input={userInput} />
         </>
     );
 }
